46f25cfa55d0f6e9553bbb0dbc643bb9034a5634
asiabosi <asia.bosi@galileiostiglia.edu.it>
2024-09-30 15:24:23 +0000

    compiti_3009

---
+++ comp_20240930.c
@@ -0,0 +1,14 @@
+/*Dati 3 numeri stampare il più piccolo, il più grande e la loro media aritmetica.*/
+#include <stdio.h>
+int main(){ //inizo
+    float numeroGrande=0;
+    float numeroPiccolo=0;
+    float media=0:
+    printf("inserisci il primo numero: ");
+    scanf("%f",&numeroGrande);
+    printf("inserisci il secondo numero: ");
+    scanf("%f",&numeroPiccolo);
+    media=(numeroGrande+numeroPiccolo)/2;
+    printf("la media è: %f", media);
+    return 0 //fine
+}
