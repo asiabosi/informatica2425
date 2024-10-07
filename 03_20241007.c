90380d8062b8e42915d1d7cdc07c86d489d01bd7
asiabosi <asia.bosi@galileiostiglia.edu.it>
2024-10-07 11:56:18 +0000

    numCifre

---
+++ 03_20241007.c
@@ -0,0 +1,14 @@
+/* DATO IL NUMERO A PIU' CIFRE STAMPARE 
+LE CIFRE HE COMPONGONO IL NUMERO*/
+
+ #include <stdio.h>
+    
+    int main(){
+        int num=0;
+        int q, r;
+        printf("inserisci un numero a più cifre:");
+        scanf("%d", &num);
+        
+
+        
+    }
